33193fcf6cab3ff5436e02fb2e6584b7b6c628dd
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-11 14:43:06 -0800

    complete lecture 100

--- starter/script.js
+++ starter/script.js
@@ -133,6 +133,7 @@ console.log(z === window.z);
 
 // Lecture 98: Regular Functions vs. Arrow Functions
 
+/*
 var firstName = 'Matilda';
 
 const justice = {
@@ -181,3 +182,60 @@ var addArrow = (a, b) => {
   return a + b;
 };
 addArrow(2, 5, 8);
+*/
+
+////////////////////////////////////
+
+// Lecture 100: Primitives vs. Objects (Primitives vs. Reference Types)
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
+
+// const me = {
+//   name: 'jonas',
+//   age: 30,
+// };
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend', friend);
+// console.log('Me', me);
+
+// primitive types
+let lastName = 'williams';
+let oldLastName = lastName;
+lastName = 'davis';
+console.log(lastName, oldLastName);
+
+// reference types
+const jessica = {
+  firstName: 'jessica',
+  lastName: 'williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'davis';
+console.log(`before marriage:`, jessica);
+console.log(`after marriage:`, marriedJessica);
+
+// marriedJessica = {};
+
+// copying objects
+const jessica2 = {
+  firstName: 'jessica',
+  lastName: 'williams',
+  age: 27,
+  family: ['alice', 'bob']
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'davis';
+
+jessicaCopy.family.push('mary');
+jessicaCopy.family.push('john');
+
+console.log(`before marriage:`, jessica2);
+console.log(`after marriage:`, jessicaCopy);
